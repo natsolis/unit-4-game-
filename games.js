537475bfa883f276b96d6ad1ef7424d8ebf184d7
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-06-04 21:33:36 -0600

    third commit

--- games.js
+++ games.js
@@ -1,14 +1,24 @@
 // Random number given to users at the beginning of the game. Might need a for loop.
-var randomNumber;
+ var randomNumber;
+ var losses;
+ var win;
+ 
+// $(document).ready(function(){
+//     var array =[];
+//     for(var i = 19; i < 120; i++){
+//         console.log("hello world")
+//          array.push()
+//     }
+     var randomNumber= Math.floor(Math.random() * 19) + 110;
+     console.log(randomNumber)
+    $("#number").text(randomNumber);
 
-var computerGuess= Math.floor(Math.random() * 50);
-$(document).ready(function(){
-
-
-});
 
 
 // The value of the crystals go here, also the function which will give them all different values.
+var crystal = Math.floor(Math.random())
+
+
 
 
 
@@ -19,3 +29,4 @@ $(document).ready(function(){
 
 
 //  Users lose if the number on the screen goes over the random number. 
+// });
