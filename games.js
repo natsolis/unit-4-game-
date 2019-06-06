8af04ee0fb90c95f460e60380d6b13f462c65cad
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-06-06 08:10:04 -0600

    almost done

--- games.js
+++ games.js
@@ -1,24 +1,62 @@
 // Random number given to users at the beginning of the game. Might need a for loop.
- var randomNumber;
- var losses;
- var win;
- 
+var randomNumber;
+var losses;
+var win;
+
 // $(document).ready(function(){
 //     var array =[];
 //     for(var i = 19; i < 120; i++){
 //         console.log("hello world")
 //          array.push()
 //     }
-     var randomNumber= Math.floor(Math.random() * 19) + 110;
-     console.log(randomNumber)
-    $("#number").text(randomNumber);
+var randomNumber = Math.floor(Math.random() * 110) + 19;
+console.log(randomNumber)
+$("#number").text(randomNumber);
 
 
 
 // The value of the crystals go here, also the function which will give them all different values.
-var crystal = Math.floor(Math.random())
+// var crystal = Math.floor(Math.random())
+for (var i = 0; i < 4; i++) {
+    var random = Math.floor(Math.random() * 12);
+    console.log(random);
+    
+    var crystal = $("<div>");
+    crystal.attr({
+        "class": 'crystal',
+        "data-random": random
+   
+    });
+    
+    $(".crystals").append(crystal);
+}
 
+resetAndStart();
 
+$(".document").on('click', 'crystal', function () {
+    var num = parseInt($(this).attr('data-random'));
+    
+    previous += num
+    c
+    onsole.log(previous)
+    if (previous > randomNumber) {
+        losses--;
+
+        $('#losses').html(losses);
+        resetAndStart();
+    }
+
+    else if (previous === randomNumber) {
+        win++;
+
+        $("#win").html(win); 
+
+        resetAndStart();
+        alert("YOU WIN!")
+
+    }
+
+});
 
 
 
