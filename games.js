b38dff0a3c39bae4d0f05702ee90a58cf7c18d53
Nathalie Solis-Maravilla <nathaliesolis-maravilla@Nathalies-MacBook-Pro.local>
2019-06-08 18:33:18 -0600

    done

--- games.js
+++ games.js
@@ -2,6 +2,9 @@
 var randomNumber;
 var losses;
 var win;
+var crystal;
+var score = ""
+
 
 // $(document).ready(function(){
 //     var array =[];
@@ -12,51 +15,140 @@ var win;
 var randomNumber = Math.floor(Math.random() * 110) + 19;
 console.log(randomNumber)
 $("#number").text(randomNumber);
+function reset(){
+    arrayOfCrystal=[];
+    addition =[];
+    // has to reset the randomNumber
+    randomNumber = Math.floor(Math.random() * 110) + 19;
+    $("#number").text(randomNumber);
+   // also has to reset the crystals and total score 
+   e();
 
-
+}
+var crystal;
+var arrayOfCrystal = [];
+var addition = [];
 
 // The value of the crystals go here, also the function which will give them all different values.
-// var crystal = Math.floor(Math.random())
-for (var i = 0; i < 4; i++) {
-    var random = Math.floor(Math.random() * 12);
-    console.log(random);
-    
-    var crystal = $("<div>");
-    crystal.attr({
-        "class": 'crystal',
-        "data-random": random
-   
-    });
-    
-    $(".crystals").append(crystal);
+// var crystal = Math.floor(Math.random()) 
+// function crystal(){
+for (var i = 1; i < 13; i++) {
+    arrayOfCrystal.push(i);
+    console.log(arrayOfCrystal);
 }
+crystal = Math.floor(Math.random() * arrayOfCrystal.length);
+arrayOfCrystal.splice((crystal - 1), 1);
 
-resetAndStart();
+crystal1 = Math.floor(Math.random() * arrayOfCrystal.length);
+arrayOfCrystal.splice((crystal1 - 1), 1);
 
-$(".document").on('click', 'crystal', function () {
-    var num = parseInt($(this).attr('data-random'));
-    
-    previous += num
-    c
-    onsole.log(previous)
-    if (previous > randomNumber) {
-        losses--;
+crystal2 = Math.floor(Math.random() * arrayOfCrystal.length);
+arrayOfCrystal.splice((crystal2 - 1), 1);
 
-        $('#losses').html(losses);
-        resetAndStart();
+crystal3 = Math.floor(Math.random() * arrayOfCrystal.length);
+arrayOfCrystal.splice((crystal3 - 1), 1);
+
+
+//  console.log(crystal);}s
+function e(){
+$("#first").on('click', function() {
+    addition.push(crystal);
+    console.log(crystal)
+    // update DOM
+    if (addition.length < 2) {
+        $("#score").text(crystal);
     }
 
-    else if (previous === randomNumber) {
-        win++;
+    else {
+        score = $("#score").text();
+        score = parseInt(score);
+        score += crystal;
+        $("#score").text(score);
+        win();
+        losses();
+       
 
-        $("#win").html(win); 
 
-        resetAndStart();
-        alert("YOU WIN!")
+
 
     }
+
 
 });
+$("#second").on('click', function () {
+
+    addition.push(crystal1);
+    console.log(crystal1)
+    if (addition.length < 2) {
+        $("#score").text(crystal1);
+    }
+
+    else {
+        score = $("#score").text();
+        score = parseInt(score);
+        score += crystal1;
+        $("#score").text(score);
+        console.log(crystal1);
+        win();
+        losses();
+       
+    }
+});
+
+
+$("#third").on('click', function () {
+    //crystal();
+    addition.push(crystal2);
+    console.log(crystal2)
+    // update DOM
+    if (addition.length < 2) {
+        $("#score").text(crystal2);
+    }
+
+    else {
+        score = $("#score").text();
+        score = parseInt(score);
+        score += crystal2;
+        $("#score").text(score);
+        console.log(crystal2);
+        win();
+        losses();
+       
+    }
+});
+
+$("#fourth").on('click', function () {
+    //crystal();
+    addition.push(crystal3);
+    console.log(crystal3)
+    // update DOM
+    if (addition.length < 2) {
+        $("#score").text(crystal3);
+    }
+
+    else {
+        score = $("#score").text();
+        score = parseInt(score);
+        score += crystal3;
+        $("#score").text(score);
+        console.log(crystal3);
+        win();
+        losses();
+      
+
+    }
+});
+
+}
+
+e();
+// resetAndStart();
+
+
+
+
+
+// resetAndStart();
 
 
 
@@ -64,7 +156,31 @@ $(".document").on('click', 'crystal', function () {
 
 
 //The user wins if the number on the screen = the random number 
+function win() {
+    if (score === randomNumber) {
+        var win = $("#wins").text();
+        win = parseInt(win);
+        win = win+1;
+        $("#wins").text(win);  
+        reset();  
+            alert("You win!");
 
+    }
+
+}
+
+
+
+function losses() {
+    if (score > randomNumber) {
+        var losses = $("#lose").text();
+        losses = parseInt(losses);
+        losses = losses+1;
+        $("#lose").text(losses);
+        reset();
+        alert(" You Lose!")
+    }
+}
 
 //  Users lose if the number on the screen goes over the random number. 
 // });
